15be5ffe29aa2f6b733c21a040271edb22e3397e
surita <suritabtc@gmail.com>
2023-10-31 00:10:19 -0500

    corregidos errores

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,14 @@ import {RouterLink} from 'vue-router';
 </script>
 
 <template>
-  <p class="font-Pixel text-3xl">Pokemon</p>
-  <div id="nav">
-      <router-link to="/">Home</router-link> 
+  <div>
+
+    <p class="font-Pixel text-3xl w-full h-full">Pokemon</p>
+    <div id="nav">
+        <router-link to="/">Home</router-link> 
+    </div>
+    <router-view />
+    <HomePage />
   </div>
-  <router-view />
-  <HomePage />
 
 </template>
